844f7a94b7ea3f04415a670fe1d37b744f316b76
Davi Borges <davi.borges@clubemontreal.com.br>
2020-12-03 13:31:53 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:animations/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ lib/screens/home/widgets/animated_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:animations/screens/home/widgets/list_data.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedListView extends StatelessWidget {
+  final Animation<EdgeInsets> listSlidePosition;
+
+  AnimatedListView({@required this.listSlidePosition});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        ListData(
+          title: "Estudar Flutter",
+          subtitle: "Com o curdo do Davi Borges",
+          image: AssetImage('images/perfil.jpg'),
+          margin: listSlidePosition.value * 3,
+        ),
+        ListData(
+          title: "Estudar Flutter",
+          subtitle: "Com o curdo do Davi Borges",
+          image: AssetImage('images/perfil.jpg'),
+          margin: listSlidePosition.value * 2,
+        ),
+        ListData(
+          title: "Estudar Flutter",
+          subtitle: "Com o curdo do Davi Borges",
+          image: AssetImage('images/perfil.jpg'),
+          margin: listSlidePosition.value * 1,
+        ),
+        ListData(
+          title: "Estudar Flutter",
+          subtitle: "Com o curdo do Davi Borges",
+          image: AssetImage('images/perfil.jpg'),
+          margin: listSlidePosition.value * 0,
+        )
+      ],
+    );
+  }
+}
